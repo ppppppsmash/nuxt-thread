06b11e0e5cf061df7162014de2aab1570b6d4a33
ppppppsmash <pppppp.work@gmail.com>
2023-07-24 21:57:49 +0900

    mod: 初期設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  pages: true,
+  modules: [
+    'nuxt-icon',
+    '@nuxtjs/tailwindcss',
+    //'@nuxtjs/supabase',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@vite-pwa/nuxt'
+  ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL
+    }
+  },
   devtools: { enabled: true }
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script steup>
+
+</script>
+
+<template>
+  <div>
+    トップ
+  </div>
+</template>
