decae2b8571213af9116013d2a303a6443fab02a
ppppppsmash <pppppp.work@gmail.com>
2023-07-24 23:26:42 +0900

    mod: post loop テスト

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
 <script steup>
-import { useUserStore } from '~/stores/user'
-const userStore = useUserStore()
+// import { useUserStore } from '~/stores/user'
+// const userStore = useUserStore()
+
+let posts = ref([])
+const isPosts = ref(true)
+const isLoading = ref(false)
+
+posts.value = [
+  {
+    name: 'ARATA',
+    image: 'https://placehold.co/100',
+    text: 'This is the title',
+    picture: 'https://placehold.co/100',
+  }
+]
 </script>
 
 <template>
   <div id="IndexPage" class="w-full overflow-auto">
     <div class="mx-auto max-w-[500px] overflow-hidden">
       <div id="Posts" class="px-4 max-w-[600px] mx-auto">
-        <div v-if="isPosts" v-for="post in posts" :key="post">
+        <div class="text-white" v-for="post in posts" :key="post">
           {{ post }}
         </div>
       </div>
