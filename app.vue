97a9dfc85dc0240be79efd0736bb3f1072c7217c
ppppppsmash <pppppp.work@gmail.com>
2023-07-29 22:05:36 +0900

    mod: CreatePost追記処理

--- app.vue
+++ app.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+</script>
+
 <template>
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <NuxtLayout>
       <NuxtPage />
+
+      <CreatePost
+        :class="[
+          {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay},
+          {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay},
+        ]"
+      />
     </NuxtLayout>
   </div>
 </template>
--- components/CreatePost.vue
+++ components/CreatePost.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { v4 as uuidv4 } from 'uuid'
 import { useUserStore } from '@/stores/user'
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 const userStore = useUserStore()
 
 // const client = useSupabaseClient()
@@ -12,8 +12,8 @@ let isLoading = ref(false)
 
 const adjustTextareaHeight = () => {
   const textarea = document.getElementById('textarea')
-  textarea.style.height = 'auto'
-  textarea.style.height = textarea.scrollHeight + 'px'
+  textarea?.style?.setProperty('height', 'auto')
+  textarea?.style?.setProperty('height', `${textarea?.scrollHeight}px`)
 }
 
 let file = ref(null)
@@ -27,15 +27,35 @@ const clearData = () => {
   fileData.value = null
 }
 
+const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+
 const onChange = () => {
-  fileDisplay.value = URL.createObjectURL(file.value.files[0])
-  fileData.value = file.value.files[0]
+  if (!fileInput?.files?.[0]) return
+
+  const selectedFile = fileInput.files[0]
+  fileDisplay.value = URL.createObjectURL(selectedFile);
+  fileData.value = selectedFile;
 }
 
 </script>
 
 <template>
   <div id="CreatePost" class="fixed z-50 bottom-0 h-full w-full overflow-hidden">
-
+    <div class="bg-black h-full w-full text-white overflow-auto">
+      <div
+        class="flex items-center justify-start py-4 max-w-[500px] mx-auto
+          border-b border-b-gray-700"
+      >
+        <button
+          class="rounded-full px-2"
+          @click="
+            userStore.isMenuOverlay = false;
+            clearData();
+          "
+        >
+          <Icon name="mdi:close" size="25" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
